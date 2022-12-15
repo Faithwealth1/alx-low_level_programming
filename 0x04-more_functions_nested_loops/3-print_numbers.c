54b114a1cb7942c777cf55230a41d186e3d8fdb8
Faithwealth1 <faithmkting@gmail.com>
2022-12-15 09:24:58 -0800

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
+ * print_numbers - prints the numbers,from 0 to 9,
  * followed by a new line
  */
 void print_numbers(void)
@@ -14,4 +14,4 @@ void print_numbers(void)
 	}
 
 	_putchar('\n');
-}	
+}
